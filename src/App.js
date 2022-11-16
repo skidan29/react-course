fadcf174e5a6a5c190b40c8734786cf97882e3c3
alexander <skidan2908@gmail.com>
2022-11-16 11:06:00 +0300

    Обучение реакт

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState('placeholder')
+
+
+    return (
+        <div className="App">
+            <div className="post">
+                <div className="post__content">
+                    <strong>JS</strong>
+                </div>
+                <div className="post__btn">
+                    <button>Read</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
