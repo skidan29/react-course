e0ce3cd889415524f4f4efce698bd4623773c5ba
alexander <skidan2908@gmail.com>
2022-12-12 17:10:38 +0300

    1.30 Модальное окно (yml)

--- src/App.js
+++ src/App.js
@@ -9,54 +9,54 @@ import {MyButton} from "./components/UI/button/MyButton";
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'JS', body: 'Лучший язык для веб'},
-        {id: 2, title: 'Python', body: 'Лучший язык для веб'},
-        {id: 3, title: 'Java', body: 'Лучший язык для веб'}
-
-    ]);
-    let [visibleModal, setVisibleModal] = useState(false);
-    const createPost = (post) => {
-        setPosts([...posts, post]);
-        setVisibleModal(false);
-    }
-
-    const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id));
-    }
-
-    const [filter, setFilter] = useState({sort: '', query: ''});
-
-    const sortedPost = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        }
-        return posts;
-
-    }, [filter.sort, posts]);
-
-    const sortedAndFilteredPost = useMemo(() => {
-        if (filter.query) {
-            return sortedPost.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
-        }
-        return [...sortedPost];
-    }, [filter.query, posts]);
+    // const [posts, setPosts] = useState([
+    //     {id: 1, title: 'JS', body: 'Лучший язык для веб'},
+    //     {id: 2, title: 'Python', body: 'Лучший язык для веб'},
+    //     {id: 3, title: 'Java', body: 'Лучший язык для веб'}
+    //
+    // ]);
+    // let [visibleModal, setVisibleModal] = useState(false);
+    // const createPost = (post) => {
+    //     setPosts([...posts, post]);
+    //     setVisibleModal(false);
+    // }
+    //
+    // const removePost = (post) => {
+    //     setPosts(posts.filter(p => p.id !== post.id));
+    // }
+    //
+    // const [filter, setFilter] = useState({sort: '', query: ''});
+    //
+    // const sortedPost = useMemo(() => {
+    //     if (filter.sort) {
+    //         return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+    //     }
+    //     return posts;
+    //
+    // }, [filter.sort, posts]);
+    //
+    // const sortedAndFilteredPost = useMemo(() => {
+    //     if (filter.query) {
+    //         return sortedPost.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
+    //     }
+    //     return [...sortedPost];
+    // }, [filter.query, posts]);
 
     return (
         <div className="App">
 
-            <MyButton onClick={() => setVisibleModal(true)}>Добавить пост</MyButton>
+            {/*<MyButton onClick={() => setVisibleModal(true)}>Добавить пост</MyButton>*/}
 
-            <MyModal visible={visibleModal} setVisible = {setVisibleModal}>
-                <PostForm create={createPost}/>
-            </MyModal>
+            {/*<MyModal visible={visibleModal} setVisible = {setVisibleModal}>*/}
+            {/*    <PostForm create={createPost}/>*/}
+            {/*</MyModal>*/}
 
-            <hr style={{margin: '12px 0'}}/>
+            {/*<hr style={{margin: '12px 0'}}/>*/}
 
-            <PostFilter filter={filter} setFilter={e => setFilter(e)}/>
+            {/*<PostFilter filter={filter} setFilter={e => setFilter(e)}/>*/}
 
 
-            <PostList posts={sortedAndFilteredPost} remove={removePost} title='Список проектов'/>
+            {/*<PostList posts={sortedAndFilteredPost} remove={removePost} title='Список проектов'/>*/}
 
         </div>
     );
